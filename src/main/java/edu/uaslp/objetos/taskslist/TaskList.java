e81829a79dcec708a24f68888e852bc3f1d92ce0
MikelBarajas38 <mikelbarajas388@gmail.com>
2023-04-28 09:56:31 -0600

    defined data structure used for tasklist

--- src/main/java/edu/uaslp/objetos/taskslist/TaskList.java
+++ src/main/java/edu/uaslp/objetos/taskslist/TaskList.java
@@ -1,6 +1,14 @@
 package edu.uaslp.objetos.taskslist;
 
+import java.util.TreeSet;
+
 public class TaskList {
+
+    private TreeSet tasklist;
+
+    public TaskList() {
+        tasklist = new TreeSet<>();
+    }
 
     public int getSize() {
         return 0;
